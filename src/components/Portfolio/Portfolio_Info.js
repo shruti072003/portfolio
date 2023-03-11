0158a6c628179abd38ebd9e2298a59e5d0a5a430
Shruti Tewaney <shruti072003@gmail.com>
2023-03-11 02:09:17 -0500

    updated project links

--- src/components/Portfolio/Portfolio_Info.js
+++ src/components/Portfolio/Portfolio_Info.js
@@ -1,16 +1,24 @@
-import IMG1 from '../../assets/portfolio1.jpg';
+import IMG1 from '../../assets/landing_page.png';
+import IMG2 from '../../assets/sociofy.png';
 import CMG_SOON from '../../assets/coming_soon.avif';
 
 const data = [
     {
         id: 1, 
         image: IMG1,
-        title: "Sociofy",
-        github: 'https://github.com/',
-        demo: 'https://youtube.com'
+        title: "Social Landing Page",
+        github: 'https://github.com/shruti072003/social-component',
+        demo: 'https://shruti072003.github.io/social-component/'
     },
     {
         id: 2, 
+        image: IMG2,
+        title: "Sociofy",
+        github: 'https://github.com/shruti072003/sociofy-frontend',
+        demo: 'github.com/shruti072003/sociofy-backend'
+    },
+    {
+        id: 3, 
         image: CMG_SOON,
         title: "Shortly - URL Shortener",
         github: 'https://github.com/shruti072003',
