7cb3b635012f05319088ce151bdfa455a2a95724
Shruti Tewaney <shruti072003@gmail.com>
2023-03-15 00:25:29 -0500

    Updates

---
+++ precache-manifest.1c1b8b8a79830d75027b6f0035ff91b9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "605fe605e1fe355dcfeb",
+    "url": "/portfolio/static/css/main.6238f665.chunk.css"
+  },
+  {
+    "revision": "605fe605e1fe355dcfeb",
+    "url": "/portfolio/static/js/main.605fe605.chunk.js"
+  },
+  {
+    "revision": "4e2543aaa66105cb2d50",
+    "url": "/portfolio/static/js/1.4e2543aa.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "d3e6bf47ee2014f178546d0d69ae3e05",
+    "url": "/portfolio/static/media/Shruti_Resume.d3e6bf47.pdf"
+  },
+  {
+    "revision": "382326db182567f8d7912ce8c96f33be",
+    "url": "/portfolio/static/media/me.382326db.png"
+  },
+  {
+    "revision": "61cbb94fc30658cfa72776a656e1b9d5",
+    "url": "/portfolio/static/media/me-about.61cbb94f.jpeg"
+  },
+  {
+    "revision": "35a23694a475dde2e06bcc5cc4cfac36",
+    "url": "/portfolio/static/media/landing_page.35a23694.png"
+  },
+  {
+    "revision": "00297f5dd62e8f683744b7a1b7672cb0",
+    "url": "/portfolio/static/media/sociofy.00297f5d.png"
+  },
+  {
+    "revision": "c94ed84dcfd1f69c1524e7b58173a3b5",
+    "url": "/portfolio/static/media/url_shortening.c94ed84d.jpg"
+  },
+  {
+    "revision": "5fc8cc44ee1decf6ed3dfeb53c2513af",
+    "url": "/portfolio/index.html"
+  }
+];
